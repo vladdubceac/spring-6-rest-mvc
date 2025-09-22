5caadefe73adc251f373b280d7e86aac80fd733a
Vlad <vladimir.dubceac@snt.md>
2025-09-22 18:29:39 +0300

    Adding project Lombok

---
+++ src/main/java/md/vladdubceac/learning/spring6restmvc/model/BeerStyle.java
@@ -0,0 +1,5 @@
+package md.vladdubceac.learning.spring6restmvc.model;
+
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
---
+++ src/test/java/md/vladdubceac/learning/spring6restmvc/Spring6RestMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package md.vladdubceac.learning.spring6restmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6RestMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
