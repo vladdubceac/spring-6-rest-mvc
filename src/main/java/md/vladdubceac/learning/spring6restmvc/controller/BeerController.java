3129da3b7ea128bec77a7504c81fd8cffbdeaf12
Vlad <vladimir.dubceac@snt.md>
2025-09-23 18:11:33 +0300

    Added logging

--- src/main/java/md/vladdubceac/learning/spring6restmvc/controller/BeerController.java
+++ src/main/java/md/vladdubceac/learning/spring6restmvc/controller/BeerController.java
@@ -1,11 +1,24 @@
 package md.vladdubceac.learning.spring6restmvc.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import md.vladdubceac.learning.spring6restmvc.model.Beer;
 import md.vladdubceac.learning.spring6restmvc.services.BeerService;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
+
     private final BeerService beerService;
+
+    public Beer getBeerById(UUID id) {
+
+        log.debug("Get Beer by ID - in container");
+
+        return beerService.getBeerById(id);
+    }
 }
--- src/main/java/md/vladdubceac/learning/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/md/vladdubceac/learning/spring6restmvc/services/BeerServiceImpl.java
@@ -1,15 +1,22 @@
 package md.vladdubceac.learning.spring6restmvc.services;
 
+import lombok.extern.slf4j.Slf4j;
 import md.vladdubceac.learning.spring6restmvc.model.Beer;
 import md.vladdubceac.learning.spring6restmvc.model.BeerStyle;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
+@Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
+
+        log.debug("Get Beer by ID - in service. ID = " + id.toString());
+
         return Beer.builder()
                 .id(id)
                 .version(1)
