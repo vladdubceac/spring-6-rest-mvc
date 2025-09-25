91c9478fce67344d69551e4cd6bf632bf43e89be
Vlad <vladimir.dubceac@snt.md>
2025-09-25 17:54:50 +0300

    Using path parameters - get by ID

--- src/main/java/md/vladdubceac/learning/spring6restmvc/controller/BeerController.java
+++ src/main/java/md/vladdubceac/learning/spring6restmvc/controller/BeerController.java
@@ -5,9 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import md.vladdubceac.learning.spring6restmvc.model.Beer;
 import md.vladdubceac.learning.spring6restmvc.services.BeerService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,19 +13,21 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
 
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
         return beerService.listBeers();
     }
 
-    public Beer getBeerById(UUID id) {
+    @RequestMapping(value = "/{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId) {
 
         log.debug("Get Beer by ID - in container");
 
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
     }
 }
