e7351ac5202e4f73ffea12896761ec112bb86ab4
Vlad <vladimir.dubceac@snt.md>
2025-10-05 00:06:07 +0300

    Added method for testing create.

--- src/test/java/md/vladdubceac/learning/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/md/vladdubceac/learning/spring6restmvc/controller/BeerControllerTest.java
@@ -1,9 +1,11 @@
 package md.vladdubceac.learning.spring6restmvc.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import md.vladdubceac.learning.spring6restmvc.model.Beer;
 import md.vladdubceac.learning.spring6restmvc.services.BeerService;
 import md.vladdubceac.learning.spring6restmvc.services.BeerServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -12,12 +14,15 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
 
 import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Slf4j
@@ -30,14 +35,22 @@ class BeerControllerTest {
     @MockitoBean
     BeerService beerService;
 
-    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+    @Autowired
+    ObjectMapper objectMapper;
+
+    BeerServiceImpl beerServiceImpl;
+
+    @BeforeEach
+    void setUp() {
+        beerServiceImpl = new BeerServiceImpl();
+    }
 
     @Test
     void testListBeers() throws Exception {
         given(beerService.listBeers()).willReturn(beerServiceImpl.listBeers());
 
         mockMvc.perform(get("/api/v1/beer")
-                .accept(MediaType.APPLICATION_JSON))
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.length()", is(3)));
@@ -47,16 +60,32 @@ class BeerControllerTest {
     void getBeerById() throws Exception {
         log.debug("Test start " + Instant.now());
 
-        Beer testBeer = beerServiceImpl.listBeers().get(0);
+        Beer testBeer = beerServiceImpl.listBeers().getFirst();
         UUID beerId = testBeer.getId();
         given(beerService.getBeerById(beerId)).willReturn(testBeer);
 
-        mockMvc.perform(get("/api/v1/beer/"+ beerId)
-                .accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/api/v1/beer/" + beerId)
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id", is(beerId.toString())))
                 .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())));
         log.debug("Test end " + Instant.now());
+    }
+
+    @Test
+    void testCreateNewBeer() throws Exception {
+        Beer beer = Beer.builder().beerName("My Beer")
+                .upc("01")
+                .version(1).price(BigDecimal.TEN).build();
+
+        given(beerService.saveNewBeer(any(Beer.class))).willReturn(beerServiceImpl.listBeers().get(1));
+
+        mockMvc.perform(post("/api/v1/beer")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(beer)))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"));
     }
 }
