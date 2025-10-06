33475a6c023032caf8364029fb9c48e66d73842b
Vlad <vladimir.dubceac@snt.md>
2025-10-06 23:03:01 +0300

    Added ResponseStatus annotation to custom exception

--- src/main/java/md/vladdubceac/learning/spring6restmvc/utils/NotFoundException.java
+++ src/main/java/md/vladdubceac/learning/spring6restmvc/utils/NotFoundException.java
@@ -1,5 +1,9 @@
 package md.vladdubceac.learning.spring6restmvc.utils;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value not found")
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
     }
